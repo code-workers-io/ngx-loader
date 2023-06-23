908918af59d7a3a0d97912104a333f76ca6084ff
Michael Berger <m.gonser@icloud.com>
2023-06-23 08:50:52 +0200

    chore: update to nx 16.3.2

--- libs/ngx-loader/jest.config.ts
+++ libs/ngx-loader/jest.config.ts
@@ -1,16 +1,18 @@
-module.exports = {
-  displayName: 'demo',
+/* eslint-disable */
+export default {
+  displayName: 'ngx-loader',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
-  coverageDirectory: '../../coverage/apps/demo',
+  globals: {},
+  coverageDirectory: '../../coverage/libs/ngx-loader',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
